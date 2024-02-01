2a79e3733b98b453fd95ee220a9693191b9ad47d
priyarajesh7696 <rameshpriyait@gmail.com>
2024-02-01 14:42:35 +0530

    code changed

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-
 const express = require('express')
 const app = express()
 const fs = require('fs')
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    let today =Date.now()
-    let time=new Date()
-fs.writeFileSync(`Date-Time/${today}.txt`,`${time}`,'utf8')
-let data = fs.readFileSync(`Date-Time/${today}.txt`,'utf8')
+    // let today =Date.now()
+    let time=new Date().toISOString()
+    let newTime = time.replace(":","-")
+    let newTime1 = newTime.replace(":","-")
+
+fs.writeFileSync(`Date-Time/${newTime1}.txt`,`${time}`,'utf8')
+let data = fs.readFileSync(`Date-Time/${newTime1}.txt`,'utf8')
   res.send(data)
-})
+  })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
